19efe67ae7c3869cacf300de7d892cb7a1f264c8
ahyun <fgrt1006@naver.com>
2024-11-04 21:23:38 +0900

    feat: add middleware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,17 @@
-export { auth as middleware } from '@/auth'
+import { auth } from '@/auth';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+    const session = await auth();
+
+    if (!session) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/mypage"],
+};
