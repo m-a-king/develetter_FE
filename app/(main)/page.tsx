3e61f0b0fdddad129289420601c5182816eb619a
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-05 17:17:59 +0900

    change page.tsx

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button'
 
 export default function MainPage() {
   const handleSubscribeClick = () => {
-    window.location.href = '/mypage'
+    window.location.href = '/mypage/subscribe'
   }
 
   return (
